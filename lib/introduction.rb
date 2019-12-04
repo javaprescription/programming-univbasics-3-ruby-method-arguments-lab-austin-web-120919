4decfdd03425f25aa0f541eab41f394dac514add
Austin Scott <austinscott4@gmail.com>
2019-12-04 01:09:44 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,7 +2,7 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_languagea(name, language)
+def introduction_with_language(name, language)
   "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
